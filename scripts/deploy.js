0283d07934afd095464476e2798d0b6febe023dd
Sean Anih <sean.anih@gmail.com>
2022-12-08 15:25:03 -0500

    connected frontend to blockchain & set up event listener

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,28 +4,29 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const [owner, user1, user2] = await ethers.getSigners();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+	//get contract
+	const contract = await ethers.getContractFactory('AddWords')
+	const AddWordsContract = await contract.deploy('first')
+	await AddWordsContract.deployed()
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	console.log(`contract deployed to: ${AddWordsContract.address}`)
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	// add words from different users
+	let tx = await AddWordsContract.connect(user1).addWord('2nd', 'this is my first message')
+	await tx.wait()
+	
+	tx = await AddWordsContract.connect(user2).addWord('3rd', 'fuckin intellisense...or whatever')
+	await tx.wait()
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+main().catch(error => {
+	console.error(error);
+	process.exitCode = 1;
 });
